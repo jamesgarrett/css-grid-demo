fd9f7d894fe33fd8e1245bf5d19bafcfd4537fee
jamesgarrett <jgsibinga@gmail.com>
2021-01-06 14:46:49 -0500

    updates to layouts

--- src/components/Live.jsx
+++ src/components/Live.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Live = () => (
   <div className="hero">
-    <h1 className="display-primary-8">Live</h1>
+    <h1 className="display-primary-8 p-y p-lg">Live</h1>
   </div>
 );
 

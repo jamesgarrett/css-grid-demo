1500aa529df24e0cde23a68b91d6d26badbe2bb0
jamesgarrett <jgsibinga@gmail.com>
2021-01-06 15:50:28 -0500

    remove watchlist and initial

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,6 @@ import { NavLink } from 'react-router-dom';
 import ForYou from '../img/icons/for-you.svg';
 import Live from '../img/icons/live.svg';
 import Search from '../img/icons/search.svg';
-import WatchList from '../img/icons/watchlist.svg';
 import AmcLogo from '../img/logos/amc.svg';
 
 const Navigation = () => (
@@ -30,18 +29,11 @@ const Navigation = () => (
           <span className="nav-text-label button-1 m-x m-xs hidden-sm hidden-md"> Live</span>
         </NavLink>
       </li>
-      <li className="nav-item">
-        <NavLink to="/watchlist">
-          <img src={WatchList} alt="Watchlist Icon" className="nav-icon" />
-          <span className="nav-text-label button-1 m-x m-xs hidden-sm hidden-md"> Watchlist</span>
-        </NavLink>
-      </li>
     </span>
     <span className="nav-bottom">
       <li className="nav-item">
         <NavLink to="/profile">
-          <span className="initial">G</span>
-          <span className="nav-text-label button-1 m-x m-xs hidden-sm hidden-md"> Profile</span>
+          <span className="nav-text-label button-1 m-x m-xs hidden-sm hidden-md">Account</span>
         </NavLink>
       </li>
     </span>

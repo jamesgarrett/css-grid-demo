9e8610bdf54bd3e3147b25bb7a3807d12ce463d7
jamesgarrett <jgsibinga@gmail.com>
2021-01-07 11:12:37 -0500

    show password example

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ShowPassword from '../img/icons/show-password.svg';
 
 const Profile = () => (
   <div className="hero m-y m-sm">
     <h1 className="display-primary-8">Profile</h1>
+    <img src={ShowPassword} alt="For You Icon" className="nav-icon" />
   </div>
 );
 
